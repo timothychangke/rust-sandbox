9c63ed3fde09c2e3fe8a690869e4416057ac328f
tim <changkaien0101@gmail.com>
2025-05-12 13:31:48 +0800

    feat(rust by practice): exercise 1

---
+++ rust by practice/variables/binding_and_mutability.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    //Exercise 1
+    let x: i32; // If variable is initialised but used, it will error
+    let y: i32; // If variable is initialised but unused, it will return a warning
+    // To bypass the warning, use an _
+    let _z: i32
+    // Assert equality macro, which will error if the equality is not met
+    assert_eq!(x, 5)
+    println!("Success!")
+
+
+}
