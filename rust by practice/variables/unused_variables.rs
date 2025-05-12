e19506f95dad209cb45292dea56517e9c95b3a7b
tim <changkaien0101@gmail.com>
2025-05-12 13:51:50 +0800

    feat(rust by practice): unused_variables

---
+++ rust by practice/variables/unused_variables.rs
@@ -0,0 +1,7 @@
+#[allow(unused_variables)]
+fn main() {
+    // This will output a warning
+    // Easiest solution is to apend an _ to the start of the variable name => _x
+    // Another solution is to use #[allow(unused_variables)] at the top of the script
+    let x = 1
+}
